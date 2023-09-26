3902daceb39e369e75a6486779b48f0bc9ad188d
IndahTriana <indah.itr.9@gmail.com>
2023-09-26 20:47:07 +0700

    membuat project CI 3 Pustaka Booking

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,14 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function Index(){
+    echo "<h1> perkenalkan </h1>";
+    echo "Nama saya Indah Triana
+    saya tinggal di daerah Tanjung Duren
+    Olahraga yang saya sukai adalah basket";
+    }
+
+}
+
+
+?>
